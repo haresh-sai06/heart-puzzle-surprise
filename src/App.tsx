aa7c8a07ab3287b4dd3a3af7dfcf6ad5deb0fe5a
haresh-sai06 <shareshsainaath@gmail.com>
2025-10-08 09:38:12 +0530

    fix: changed the routing

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import Album from "./components/Album";
 
+
 const queryClient = new QueryClient();
 
 const App = () => (
@@ -18,6 +19,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/album" element={<Album />} />
+          <Route path="/home" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
